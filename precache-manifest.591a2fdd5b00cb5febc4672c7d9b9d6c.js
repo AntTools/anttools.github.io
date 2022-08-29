01cc16c3d25777da2078f60e2b0b41a8ffcd442a
AntTools <anttools@users.noreply.github.com>
2022-08-29 16:48:07 +0000

    Updates

---
+++ precache-manifest.591a2fdd5b00cb5febc4672c7d9b9d6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ddae72d7f71c5941b06",
+    "url": "/static/css/main.3236ddda.chunk.css"
+  },
+  {
+    "revision": "3ddae72d7f71c5941b06",
+    "url": "/static/js/main.13a3df98.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6fed17cf550ba3dc0aa0",
+    "url": "/static/js/2.3448b4c4.chunk.js"
+  },
+  {
+    "revision": "03f046c1ba5380072c1be7f61e74d08c",
+    "url": "/index.html"
+  }
+];
